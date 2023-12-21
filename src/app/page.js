ceb0be4fab19ac58d121cb47a41c6b7d89689861
jcobby <justicecobbinah19@gmail.com>
2023-12-21 21:55:59 +0000

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+   <div className="bg-blue-950 h-screen justify-center flex">
+    <div className="w-[40rem] px-8 py-7 h-24  bg-white rounded-lg absolute border top-40  ">
+      <div className="w-64">
+      <div className=" justify-end text-2xl font-bold text-blue-900">To-Do List</div>
+
+      </div>
+    </div>
+   </div>
+  )
+}
